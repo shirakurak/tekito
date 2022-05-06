36b583dd3787d5940f18f9cae5d0ee775f055ef5
surakura@hotmail.co.jp <shirakura@SHIRAKURAnoMacBook-Pro.local>
2022-05-06 20:10:51 +0900

    add some area

--- waribicanner/waribicanner.js
+++ waribicanner/waribicanner.js
@@ -1,4 +1,15 @@
 'use strict';
+
+const cameraButton = document.getElementById('camera');
+const resultDivided = document.getElementById('result-area');
+const tweetDivided = document.getElementById('tweet-area');
+
+cameraButton.onclick = () => {
+  console.log('ボタンが押されました');
+  // 計算結果表示エリアの作成
+  // ツイートエリアの作成
+}
+
 const answers = [
   '割引はありませんでした。',
   'すごい',
@@ -17,5 +28,6 @@ function sum(arr) {
   return sum;
 }
 
+// test code
 console.log(sum([1, 2]));
 console.assert(sum([1, 2]) == 3);
