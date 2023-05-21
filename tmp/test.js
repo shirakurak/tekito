f2a18e7a9d838173462a7c37e6786da8971a0ed3
shirakurak <surakura@hotmail.co.jp>
2023-05-21 15:10:55 +0900

    up

--- tmp/test.js
+++ tmp/test.js
@@ -6,7 +6,8 @@ const puppeteer = require('puppeteer');
 (async () => {
   console.log("ページを開きます");
 
-  const browser = await puppeteer.launch();
+  // const browser = await puppeteer.launch({ headless: "new" });
+  const browser = await puppeteer.launch({ headless: false });  // 実際に開く場合
   const page = await browser.newPage();
   await page.goto('https://shirakurak.github.io/')
   await browser.close();
