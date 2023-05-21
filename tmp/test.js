f7f8a379bdbf44a139edf1eb4315b3578f2b8b02
shirakurak <surakura@hotmail.co.jp>
2023-05-21 15:15:45 +0900

    up

--- tmp/test.js
+++ tmp/test.js
@@ -9,8 +9,21 @@ const puppeteer = require('puppeteer');
   // const browser = await puppeteer.launch({ headless: "new" });
   const browser = await puppeteer.launch({ headless: false });  // 実際に開く場合
   const page = await browser.newPage();
-  await page.goto('https://shirakurak.github.io/')
-  await browser.close();
+  await page.goto('https://shirakurak.github.io/');
+  console.log("開きました");
 
-  console.log("開きました")
+  console.log("localStrageの内容を取得します");
+  const localStrageData = await page.evaluate(() => {
+    const json = {};
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      json[key] = localStorage.getItem(key);
+    }
+    return json;
+  });
+
+  console.log(localStrageData);
+
+  // await browser.close();
+
 })();
